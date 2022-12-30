e7155d4506eb9ab7a141849e7090aaee6580a09d
Keivan Ipchi Hagh <keivanipchihagh@gmail.com>
2022-12-30 17:41:12 +0330

    move authentication project and adjust to the new one

---
+++ httpmonitor/auth/api.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import login
+from knox.auth import TokenAuthentication
+from rest_framework import permissions, generics
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.views import LoginView as KnoxLoginView
+from knox.models import AuthToken
+from .serializers import UserSerializer
+from rest_framework.response import Response
+
+from auth.models import User
+
+
+class LoginView(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+    authentication_classes = [TokenAuthentication]
+
+    def post(self, request, format = None):
+        serializer = AuthTokenSerializer(data = request.data)
+        serializer.is_valid(raise_exception = True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginView, self).post(request, format = None)        
+
+
+class RegisterApi(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = UserSerializer
+    authentication_classes = [TokenAuthentication]        
